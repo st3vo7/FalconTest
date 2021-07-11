b6ed53d30193be2f11402e78d2037983e461e019
st3vo7 <stefanzm@gmail.com>
2021-07-11 22:43:59 +0200

    dockerized

--- code/setup.py
+++ code/setup.py
@@ -6,7 +6,7 @@ REQUIREMENTS = [line.strip() for line in open(os.path.join(ROOT_DIR, "requiremen
 
 setuptools.setup(
     name="smallapp",
-    version='0.0.0',
+    version='0.0.1',
     packages=setuptools.find_packages(),
     license="MIT",
     python_requires=">=3.7",
